67d7dc0900eeb067376d94f00f6dddec143997fa
asianfish <75399717+asianfish62@users.noreply.github.com>
2021-01-08 15:41:54 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ this is a game where you have to
 row_num = [0,1,2,3,4]
 left_columns = [0,1]
 right_columns = [3,4]
+score = 0
 side_on = ""
 #turns on far 2 columns based on 1st parameter 
 #input left_parameter for left, right_parameter for right
@@ -16,18 +17,41 @@ def turn_on_side(side):
         side_on = "left"
     else:
         side_on = "right"
-        
+
+#turns off all lights
 def turn_off_all():
     for row in row_num:
         for column in row_num: 
             led.unplot(row, column)
 
-turn_on_side(left_columns)
-basic.pause(1000)
-turn_off_all()
+#picks either the list left_columns or right_columns randomly
+def random_side():
+    random = randint(0, 1)
+    if random == 1:
+        return left_columns
+    else:
+        return right_columns
+
+
+input.running_time()
+input.button_is_pressed(Button.A)
 
 input_window = 2000
 time_decay = 50
 minimim_time = 500
+game_over = False
 while True:
-    input.button_is_pressed(Button.A)
+    random = random_side()
+    turn_on_side(random)
+    basic.pause(input_window)
+    
+    
+    #checks for game_over
+    if game_over == True:
+        break
+
+    #time decay
+    if input_window >500:
+        input_window -= time_decay
+    turn_off_all()
+        
--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
+let random: number[];
 /** this is a game where you have to */
 let row_num = [0, 1, 2, 3, 4]
 let left_columns = [0, 1]
 let right_columns = [3, 4]
+let score = 0
 let side_on = ""
 // turns on far 2 columns based on 1st parameter 
 // input left_parameter for left, right_parameter for right
@@ -20,6 +22,7 @@ function turn_on_side(side: number[]) {
     
 }
 
+// turns off all lights
 function turn_off_all() {
     for (let row of row_num) {
         for (let column of row_num) {
@@ -28,12 +31,36 @@ function turn_off_all() {
     }
 }
 
-turn_on_side(left_columns)
-basic.pause(1000)
-turn_off_all()
+// picks either the list left_columns or right_columns randomly
+function random_side(): number[] {
+    let random = randint(0, 1)
+    if (random == 1) {
+        return left_columns
+    } else {
+        return right_columns
+    }
+    
+}
+
+input.runningTime()
+input.buttonIsPressed(Button.A)
 let input_window = 2000
 let time_decay = 50
 let minimim_time = 500
+let game_over = false
 while (true) {
-    input.buttonIsPressed(Button.A)
+    random = random_side()
+    turn_on_side(random)
+    basic.pause(input_window)
+    // checks for game_over
+    if (game_over == true) {
+        break
+    }
+    
+    // time decay
+    if (input_window > 500) {
+        input_window -= time_decay
+    }
+    
+    turn_off_all()
 }
